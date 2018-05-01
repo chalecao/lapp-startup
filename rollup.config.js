af5dab8cb7c5dcd4cc73d7c1d3110d3622829ef6
haomou.chh <haomou.chh@alibaba-inc.com>
2018-05-01 15:26:07 +0800

    update package

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-//npm install rollup rollup-plugin-babel babel-core babel-preset-env --save-dev
+//npm install rollup rollup-plugin-babel babel-core babel-preset-env babel-plugin-external-helpers --save-dev
 //npm install rollup-plugin-commonjs rollup-plugin-node-resolve --save-dev
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 export default [{
     input: 'src/module/main.js',
     output: {
@@ -20,7 +20,8 @@ export default [{
             "plugins": [
                 ["transform-react-jsx", {
                     "pragma": "l"
-                }]
+                }],
+                "external-helpers"
             ]
         }),
         resolve(),
